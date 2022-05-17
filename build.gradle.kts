f7d7d97d24061f0d5e516d196ff0cd81bc6c9e0c
GlebSolovev <lionyoung@mail.ru>
2022-05-17 09:25:55 +0300

    Use latest gradle version and generate wrapper files

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,9 @@ dependencies {
     // flink logging
     implementation("org.slf4j:slf4j-api:1.7.36")
     implementation("org.slf4j:slf4j-log4j12:1.7.36")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 java {
@@ -35,4 +38,21 @@ java {
             java.setSrcDirs(listOf("src/test"))
         }
     }
+}
+
+tasks.compileJava {
+    options.release.set(11)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("ru.hse.flinkanomaly.Main")
+}
+
+tasks.wrapper {
+    distributionType = Wrapper.DistributionType.ALL
+    gradleVersion = "7.4.2"
 }
